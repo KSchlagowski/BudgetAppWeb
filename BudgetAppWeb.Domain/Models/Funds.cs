1c04ba92b98f231cf048518a8a8cef4931730be0
KSchlagowski <mediewilnp@gmail.com>
2021-05-01 15:39:13 +0200

    Create models

--- BudgetAppWeb.Domain/Models/Funds.cs
+++ BudgetAppWeb.Domain/Models/Funds.cs
@@ -2,7 +2,7 @@ namespace BudgetAppWeb.Domain.Models
 {
     public class Funds
     {
-        public int Id { get; set; } = 0;
+        public int Id { get; set; }
         public decimal IrregularExpensesFundBalance{ get; set; } = 0;
         public decimal EmergencyFundBalance { get; set; } = 0;
         public decimal SecurityFundBalance { get; set; } = 0;
